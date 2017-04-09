d52bc5b2c3634fde24b646d77f85df0bacc1377d
2ger <15577729055@qq.com>
2017-04-09 18:43:14 +0800

    精减首页文件

--- public_html/w7/we7/data/tpl/web/default/ewei_vote/2_common.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php defined('IN_IA') or exit('Access Denied');?><script language='javascript' src='../addons/ewei_vote/style/jquery.gcjs.js'></script>
-<script language='javascript' src='../addons/ewei_vote/style/jquery.form.js'></script>
-<script language='javascript' src='../addons/ewei_vote/style/tooltipbox.js'></script>
-<link href="../addons/ewei_vote/style/common.css" rel="stylesheet"/>
